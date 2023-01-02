0ffbc1269b777eebbf89533c3fcb5aa76de76281
parkjooyoung <mirkduddl@naver.com>
2023-01-02 21:42:48 +0900

    section4 : 슬라이드 기능 추가하기

--- src/components/Row.js
+++ src/components/Row.js
@@ -11,7 +11,6 @@ const Row = ({ isLargeRow, title, id, fetchUrl }) => {
 
   const fetchMovieData = async () => {
     const request = await axios.get(fetchUrl);
-    console.log(request);
     setMovies(request.data.results);
   };
 
@@ -21,24 +20,37 @@ const Row = ({ isLargeRow, title, id, fetchUrl }) => {
 
       <div className="slider">
         <div className="slider__arrow-left">
-          <span className="arrow">{"<"}</span>
+          <span
+            className="arrow"
+            onClick={() => {
+              document.getElementById(id).scrollLeft -= window.innerWidth - 80;
+            }}
+          >
+            {"<"}
+          </span>
         </div>
 
         <div id={id} className="row__posters">
-          {movies.map((movie) => (
-            <img
-              key={movie.id}
-              className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-              src={`https://image.tmdb.org/t/p/original/${
-                isLargeRow ? movie.poster_path : movie.backdrop_path
-              }`}
-              alt={movie.name}
-            />
-          ))}
+          {movies &&
+            movies.map((movie) => (
+              <img
+                key={movie.id}
+                className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                src={`https://image.tmdb.org/t/p/original/${
+                  isLargeRow ? movie.poster_path : movie.backdrop_path
+                }`}
+                alt={movie.name}
+              />
+            ))}
         </div>
 
         <div className="slider__arrow-right">
-          <span className="arrow">{`>`}</span>
+          <span
+            className="arrow"
+            onClick={() => {
+              document.getElementById(id).scrollLeft += window.innerWidth - 80;
+            }}
+          >{`>`}</span>
         </div>
       </div>
     </section>
